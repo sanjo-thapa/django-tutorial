f227959f2bf2427cd6c3dc9201b43162e964a105
erisamsai <103209376+sanjo-thapa@users.noreply.github.com>
2023-10-27 21:24:51 +0100

    removed comments

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
-#from dotenv import load_dotenv, dotenv_values
+from dotenv import load_dotenv, dotenv_values
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#load_dotenv()
+load_dotenv()
 SECRET_KEY = os.environ['SECRET']
 
 
